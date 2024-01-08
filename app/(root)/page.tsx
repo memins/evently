ac142d9db773486bdf56d719a4e24e8cedfc53ad
Emin Sahin <emin.sahin@vizitest.com>
2024-01-08 23:25:32 +0100

    Add new files and update dependencies

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
+      <h1 className="text-4xl">Evently</h1>
       <Button>Click me</Button>
     </main>
   );
