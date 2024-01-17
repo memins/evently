725dc261cd97a383f9a25dc01335c0c6ccde97e3
Emin Sahin <emin.sahin@vizitest.com>
2024-01-17 22:57:19 +0100

    Update translations and UI labels

--- constants/index.ts
+++ constants/index.ts
@@ -1,27 +1,27 @@
 export const headerLinks = [
   {
-    label: 'Home',
-    route: '/',
+    label: "Ana Sayfa",
+    route: "/",
   },
   {
-    label: 'Create Event',
-    route: '/events/create',
+    label: "Etkinlik ekle",
+    route: "/events/create",
   },
   {
-    label: 'My Profile',
-    route: '/profile',
+    label: "Profil",
+    route: "/profile",
   },
-]
+];
 
 export const eventDefaultValues = {
-  title: '',
-  description: '',
-  location: '',
-  imageUrl: '',
+  title: "",
+  description: "",
+  location: "",
+  imageUrl: "",
   startDateTime: new Date(),
   endDateTime: new Date(),
-  categoryId: '',
-  price: '',
+  categoryId: "",
+  price: "",
   isFree: false,
-  url: '',
-}
+  url: "",
+};
